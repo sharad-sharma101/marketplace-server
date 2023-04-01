00016e12e7e879fbde34ceeaae3510b5e8846030
Sharad Sharma <106671710+sharad-sharma101@users.noreply.github.com>
2023-04-01 17:49:59 +0530

    fix cros issue

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routers/user-route';
 import carsRouter from './routers/car-route';
+var cors = require('cors')
 
 const app = express();
-
+app.use(cors()) 
 mongoose.connect('mongodb+srv://sharadsharma8058947:MitRPx9TqYBEpviH@cluster0.qs0zzsg.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>app.listen(5000)).then(
     ()=> console.log("mongoose and server at 5000 is connect")
